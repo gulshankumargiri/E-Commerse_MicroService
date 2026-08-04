0345c860e5e7c67bf9317edda03bc8e90b6d0095
Gulshan Kumar giri <golukumarrampur24@gmail.com>
2026-08-04 19:22:13 +0530

    final changes , and all the properties file added

---
+++ E_Commerce_Project/Config-serverr/src/main/java/com/config_server/ConfigServerrApplication.java
@@ -0,0 +1,13 @@
+package com.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerrApplication.class, args);
+	}
+
+}
